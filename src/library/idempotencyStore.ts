c1400b5943ecbf4a3249d7726ead1022a3b1c70b
Marino <antonio.Marino@rockybrands.com>
2026-07-29 11:24:08 -0400

    duplicate testing

--- src/library/idempotencyStore.ts
+++ src/library/idempotencyStore.ts
@@ -5,10 +5,17 @@ const TTL_SECONDS = 60 * 60 * 24 * 7; // 7 days
 export async function tryMarkProcessed(
   key: string
 ): Promise<boolean> {
+
+  const existing = await redis.get(key);
+
+  console.log("REDIS EXISTING:", key, existing);
+
   const result = await redis.set(key, 1, {
     nx: true,
     ex: TTL_SECONDS,
   });
+
+  console.log("REDIS SET RESULT:", result);
 
   return result === "OK";
 }
